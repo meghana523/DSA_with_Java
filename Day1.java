bb62f0613c35dc024e98e2fd4541b631770cb1fb
P.Lakshmi Meghana <lakshmimeghana58@gmail.com>
2026-06-15 10:23:49 +0000

    Commit

--- Day1.java
+++ Day1.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
 class Day1
 {
     public static void main(String[] args)
     {
-        
+            /*//Even Odd
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+            int a = sc.nextInt();
+            if((a & 1) == 0)
+            {
+                System.out.println("Even");
+            }
+            else
+            {
+                System.out.println("Odd");
+            }*/
+           Scanner sc = new Scanner(System.in);
+           System.out.println("Enter a number");
+           int n = sc.nextInt();
+           for (int i = 1; i <= n; i++)
+           {
+                System.out.println("Enter a number");
+                int a = sc.nextInt();
+                if((a & 1 )== 0)
+                {
+                   System.out.println("Even");
+                }
+                else
+                {
+                     System.out.println("Odd");
+                }
+           }
     }
 }
